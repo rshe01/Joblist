f66a5078be8bfb744583f5c6a432bb5606cc67b8
richardsheWM <rshe@email.wm.edu>
2021-01-18 10:18:07 -0500

    make scraped content show up

---
+++ joblist/scraper/bs.py
@@ -0,0 +1,65 @@
+import requests 
+from bs4 import BeautifulSoup
+
+def bs(url):
+    url = "https://www.indeed.com/jobs?q=Computer+Science+Internship"
+    info = []
+    titles = []
+    locations = []
+    companies = []
+    summaries = []
+
+    #makes get request for the page we are looking at 
+    page = requests.get(url)
+
+    #BS parses the html
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    elem = soup.find_all("div",class_='jobsearch-SerpJobCard')
+
+    def title(elem):
+        title_elem = elem.find("h2", class_='title')
+        title = title_elem.find("a", class_='jobtitle').text.strip()
+        return title
+
+    def location(elem):
+        rating_location_elem = elem.find('div', class_='sjcl')
+        location = rating_location_elem.find("span", class_='location').text.strip()
+        return location
+        
+    #rating is funky...
+    def rating(elem):
+        rating_location_elem = elem.find('div', class_='sjcl')
+        rating = rating_location_elem.find("span", class_='ratingsContent')
+        return rating
+
+    def company(elem):
+        rating_location_elem = elem.find('div', class_='sjcl')
+        company = rating_location_elem.find("span", class_='company').text.strip()
+        return company
+
+    #does not work since salarySnippet does not exist in the soup. wtf??
+    def salary(elem):
+        salary_elem = elem.find('div', class_='salarySnippet holisticSalary')
+        salary = salary_elem.text.strip()
+        return salary
+
+    def summary(elem):
+        summary_elem = elem.find("div", class_='summary')
+        summary = summary_elem.text.strip()
+        return summary
+        
+    for job_elem in elem:
+        titles.append(title(job_elem))
+        locations.append(location(job_elem))
+        #job_set.append(rating(job_elem))
+        companies.append(company(job_elem))
+        #job_set.append(salary(job_elem))
+        summaries.append(summary(job_elem))
+        
+    info.append(titles)
+    info.append(locations)
+    info.append(companies)
+    info.append(summaries)
+    print(info)
+    return info
--- joblist/scraper/views.py
+++ joblist/scraper/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .bs import bs;
 # Create your views here.
 
 def index(request):
-    return render(request, 'page.html')
+    context={
+        "content":bs("https://www.indeed.com/jobs?q=Computer+Science+Internship"),
+    }
+    return render(request, 'page.html', context=context)
 
