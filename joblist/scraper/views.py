2917c495529ac0a92ef446b4dbb2c72b6ac66d19
richardsheWM <rshe@email.wm.edu>
2021-01-23 23:15:35 -0500

    creating cool footer

--- joblist/scraper/views.py
+++ joblist/scraper/views.py
@@ -4,7 +4,6 @@ from .callingBS import big;
 def index(request):
     context={
         "content":big(),
-        "loopLength":range(15),
     }
     return render(request, 'page.html', context=context)
 
